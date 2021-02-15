cde46546658c983d93e53088ce6e047df02eff1b
A.rudd99 <66541290+dudu2263@users.noreply.github.com>
2021-02-15 14:23:00 +0000

    initial commit

---
+++ app.py
@@ -0,0 +1,53 @@
+import speech_recognition as sr 
+import pyttsx3
+import pywhatkit
+import datetime
+import wikipedia 
+
+listener = sr.Recognizer()
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
+
+
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
+
+def take_command():
+    try: 
+        with sr.Microphone() as source:
+            print('listening...')
+            voice = listener.listen(source)
+            command = listener.recognize_google(voice)
+            command = command.lower()
+            if 'alexa' in command:
+                command = command.replace('alexa', '')
+                print(command)
+    except: 
+        pass 
+    return command 
+
+
+
+def run_alexa():
+    command = take_command()
+    print(command)
+    if 'play' in command: 
+        song = command.replace('play', '')
+        talk('playing' + song)
+        pywhatkit.playonyt(song)
+    elif 'time' in command: 
+        time = datetime.datetime.now().strftime('%H:%M')
+        print(time)
+        talk('Current time is ' + time)
+    elif 'search' in command:
+        person = command.replace('search', '')
+        info = wikipedia.summary(person, 1)
+        print(info)
+        talk(info)
+    else: 
+        talk('Please say the command again.')
+
+while True: 
+    run_alexa()
